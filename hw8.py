8ef8d53e46f9801a3181fe37a49deb559da27f6f
UstNana <41002698+UstNana@users.noreply.github.com>
2019-02-26 11:00:11 +0300

    Create hw8.py

---
+++ hw8.py
@@ -0,0 +1,22 @@
+k = 0
+cookbook = {}
+big_list = []
+with open("HW8.txt") as f:
+    while True:
+        meal = f.readline().strip()
+        list_meal = meal.split()
+        quantity = f.readline().rstrip('\n')
+        h = quantity.split()
+        for i in h:
+            i = int(i)
+
+            for k in range(i):
+                list_1 = f.readline().strip()
+                l_list_1 = list_1.split()
+                big_list.append({'ingridient_name': l_list_1[0], 'quantity': l_list_1[2], 'measure': l_list_1[4]})
+                for eat in list_meal:
+                  if eat not in cookbook.keys():
+                    cookbook[eat] = big_list
+            print (cookbook)
+
+            f.readline()
